3d1247491652181b71276dd37510db1eaa6db0bc
e-luu <league@WTS13.attlocal.net>
2018-07-23 18:26:21 -0700

    league invaders ip

---
+++ src/leagueInvaders/LeagueInvaderRunner.java
@@ -0,0 +1,13 @@
+package leagueInvaders;
+
+import javax.swing.JFrame;
+
+public class LeagueInvaderRunner {
+	JFrame frame=new JFrame();
+	final int width=500;
+	final int hieght=800;
+public static void main(String[]args) {
+
+	
+}
+}
