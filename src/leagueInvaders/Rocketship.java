52100c8b51292db223d04c5a75ce1e050c840c14
cheetah676 <league@iMac.attlocal.net>
2018-08-19 11:27:25 -0700

    league invaders ip

--- src/leagueInvaders/Rocketship.java
+++ src/leagueInvaders/Rocketship.java
@@ -4,9 +4,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Rocketship extends GameObject{
-
+int speed;
 	Rocketship(int x, int y, int width, int height) {
 		super(x, y, width, height);
+		speed=5;
 		// TODO Auto-generated constructor stub
 	}
 void update(){
