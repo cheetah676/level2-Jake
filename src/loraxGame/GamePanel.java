a731479360ed55900b8bf0134c6fd749edac4e5d
cheetah676 <league@iMac-2.attlocal.net>
2018-11-04 11:26:13 -0800

    need to work on tax guy timing

--- src/loraxGame/GamePanel.java
+++ src/loraxGame/GamePanel.java
@@ -9,11 +9,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import leagueInvaders.Aliens;
+import leagueInvaders.LeagueInvaderRunner;
 import leagueInvaders.ObjectManager;
 import leagueInvaders.Rocketship;
 
@@ -25,15 +28,23 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	boolean drawTree=true;
 	boolean drawTree2=false;
 Timer timer=new Timer(1000/60, this);
+long taxTimer=0;
+long otherTimer=0;
+long idkTimer=0;
+int taxGuyVisit=15000;
+int taxGuyLeave=18000;
+int second=1000;
+int i=15;
 Font font;
 Font enter;
 Font space;
 Font tax;
+Font nextTax;
 Lorax lorax=new Lorax(250, 200, 100, 100);
 Tree tree=new Tree(220, 20, 200, 200);
 Rock rock=new Rock(350, 200, 200, 100);
 Tree2 tree2=new Tree2(300, 100, 200, 200);
-TaxGuy taxGuy=new TaxGuy(400, 300, 200, 200);
+TaxGuy taxGuy=new TaxGuy(800, 300, 200, 200);
 public static BufferedImage loraxImg;
 public static BufferedImage rockImg;
 public static BufferedImage treeImg;
@@ -44,12 +55,13 @@ GamePanel(){
 	enter=new Font("Arial", Font.PLAIN, 24);
 	space=new Font("Arial", Font.PLAIN, 24);
 	tax=new Font("Arial", Font.PLAIN, 24);
+	nextTax=new Font("Arial", Font.PLAIN, 24);
     try {
         loraxImg = ImageIO.read(this.getClass().getResourceAsStream("lorax.png"));
         rockImg = ImageIO.read(this.getClass().getResourceAsStream("rock.png"));
         treeImg = ImageIO.read(this.getClass().getResourceAsStream("tree.png"));
         tree2Img = ImageIO.read(this.getClass().getResourceAsStream("tree2.png"));
-        taxGuyImg = ImageIO.read(this.getClass().getResourceAsStream("taxGuy.jpg"));
+        taxGuyImg = ImageIO.read(this.getClass().getResourceAsStream("taxGuy.png"));
 } catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
@@ -65,6 +77,14 @@ void updateGameState() {
 	tree.update();
 	rock.update();
 	tree2.update();
+	if (System.currentTimeMillis() - taxTimer >= taxGuyVisit) {
+		taxGuy.x-=200;
+		taxTimer = System.currentTimeMillis();
+	}
+	if (System.currentTimeMillis() - otherTimer >= taxGuyLeave) {
+		taxGuy.x+=200;
+		otherTimer = System.currentTimeMillis();
+	}
 }
 void updateEndState(){
 	
@@ -78,7 +98,7 @@ void drawMenuState(Graphics g) {
 	g.setFont(enter);
 	g.drawString("Press ENTER to start", 265, 250);
 	g.setFont(space);
-	g.drawString("Press Space for instructions", 240, 400);
+	g.drawString("Press H for instructions", 240, 400);
 }
 void drawGameState(Graphics g) {
 	g.setColor(Color.GREEN);
@@ -92,6 +112,16 @@ void drawGameState(Graphics g) {
 	if(drawTree2==true) {
 		tree2.draw(g);
 	}
+	if (System.currentTimeMillis() - idkTimer >=1000) {
+		i-=1;
+idkTimer=System.currentTimeMillis();
+	}
+	g.setColor(Color.BLUE);
+	g.setFont(nextTax);
+	g.drawString("Next tax come in " +i+ " seconds", 50, 50);
+		if(i<0) {
+			i=15;
+		}
 }
 void drawEndState(Graphics g) {
 	g.setColor(Color.RED);
